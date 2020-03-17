4aba7cc2b67ac6ea2e55b6c48af0efcb04d2de0a
Andraz Vrhovec <andraz@koofr.net>
2020-03-17 15:40:41 +0100

    more resilent import/export

--- import.py
+++ import.py
@@ -27,13 +27,23 @@ def parse_date(datestr):
 
 
 def cell_index(letter):
+    # ord(a) = 97
     letter = letter.lower()
     if len(letter) == 1:
-        return ord(letter) - ord("a")
+        return ord(letter) - 97
     elif len(letter) == 2:
-        return (ord(letter[0]) - ord("a")) * 26 + ord(letter[1]) - ord("a") + 26
+        return (ord(letter[0]) - 97) * 26 + ord(letter[1]) - 97 + 26
     else:
         raise Exception("Unsupported cell index")
+
+
+def col_name(idx):
+    i2 = idx // 26
+    i1 = idx % 26
+    if i2 > 0:
+        return "{}{}".format(chr(97 + i2), chr(97 + i1))
+    else:
+        return chr(97 + i1)
 
 
 def pad(lst, l):
@@ -98,7 +108,7 @@ def import_stats(filename="covid19-slovenia-full.csv"):
         sheet.values()
         .get(
             spreadsheetId="1N1qLMoWyi3WFGhIpPFzKsFmVE0IwNP3elb_c18t2DwY",
-            range="Statistika!A3:BP",
+            range="Statistika!A2:BS",
         )
         .execute()
     )
@@ -109,6 +119,23 @@ def import_stats(filename="covid19-slovenia-full.csv"):
     else:
         with open(filename, "w", newline="") as csvfile:
 
+            # find columns
+            headers = values[0]
+            COL_DAY_OF_OUTBREAK = headers.index("dan")
+            COL_DATE = headers.index("datum")
+            COL_PANDEMIC_PHASE = headers.index("datum")
+            COL_TESTS_PERFORMED_CUM = headers.index("testov")
+            COL_TESTS_PERFORMED_DAY = headers.index("testov na dan")
+            COL_POSITIVE_TESTS_CUM = headers.index("potrjeni")
+            COL_POSITIVE_TESTS_DAY = headers.index("novi potrjeni")
+            COL_COUNT_IN_CARE = headers.index("v zdr. oskrbi")
+            COL_COUNT_IN_HOSPITAL = headers.index("hospitalizirano")
+            COL_COUNT_IN_ICU = headers.index("intenzivna nega ICU")
+            COL_COUNT_IN_ICU_BAD = headers.index("kritično stanje")
+            COL_DEATHS_CUM = headers.index("umrli")
+
+            regions = get_range(headers, "AA", "AM")
+            facilities = get_range(headers, "BL", "BR")
             age_groups = ["0-15", "16-29", "30-49", "50-59", "60+"]
 
             fieldnames = (
@@ -125,93 +152,70 @@ def import_stats(filename="covid19-slovenia-full.csv"):
                     "count_in_icu",
                     "count_in_icu_bad",
                     "deaths_cum",
-                    "region_lj",
-                    "region_nm",
-                    "region_ce",
-                    "region_mb",
-                    "region_kr",
-                    "region_po",
-                    "region_kp",
-                    "region_ms",
-                    "region_ng",
-                    "region_sg",
-                    "region_kk",
-                    "region_foreign",
-                    "region_unknown",
                 ]
+                + list(map(lambda g: "region_{}".format(g), regions))
                 + list(map(lambda g: "age_{}".format(g), age_groups))
                 + list(map(lambda g: "female_age_{}".format(g), age_groups))
                 + list(map(lambda g: "male_age_{}".format(g), age_groups))
                 + ["source_ita", "source_aut", "source_esp", "source_slo"]
-                + [
-                    "workers_ukc_lj",
-                    "workers_ukc_mb",
-                    "workers_zd_m",
-                    "workers_b_ms",
-                    "workers_b_ng",
-                ]
+                + list(map(lambda g: "facility_{}".format(g.lower()), facilities))
             )
 
             writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-
             writer.writerow(fieldnames)
-            # print(len(fieldnames), fieldnames)
 
-            for row in values:
-                day_of_outbreak = row[cell_index("A")]
-                date = parse_date(row[cell_index("B")])
-                pandemic_phase = row[cell_index("C")]
-                tests_performed_cum = row[cell_index("D")]
-                tests_performed_day = row[cell_index("F")]
-                positive_tests_cum = row[cell_index("H")]
-                positive_tests_day = row[cell_index("J")]
-                count_in_care = row[cell_index("L")]
-                count_in_hospital = row[cell_index("M")]
-                count_in_icu = row[cell_index("N")]
-                count_in_icu_bad = row[cell_index("O")]
-                deaths_cum = row[cell_index("P")]
+            for row in values[1:]:
+                row = pad(row, len(headers))
+                try:
+                    day_of_outbreak = row[COL_DAY_OF_OUTBREAK]
+                    date = parse_date(row[COL_DATE])
+                    pandemic_phase = row[COL_PANDEMIC_PHASE]
+                    tests_performed_cum = row[COL_TESTS_PERFORMED_CUM]
+                    tests_performed_day = row[COL_TESTS_PERFORMED_DAY]
+                    positive_tests_cum = row[COL_POSITIVE_TESTS_CUM]
+                    positive_tests_day = row[COL_POSITIVE_TESTS_DAY]
+                    count_in_care = row[COL_COUNT_IN_CARE]
+                    count_in_hospital = row[COL_COUNT_IN_HOSPITAL]
+                    count_in_icu = row[COL_COUNT_IN_ICU]
+                    count_in_icu_bad = row[COL_COUNT_IN_ICU_BAD]
+                    deaths_cum = row[COL_DEATHS_CUM]
 
-                regional_data = get_range(row, "Z", "AL")
+                    regional_data = get_range(row, "AA", "AM")
 
-                by_age = get_range(row, "AN", "AR")
-                by_age_female = get_range(row, "AT", "AX")
-                by_age_male = get_range(row, "AZ", "BD")
+                    by_age = get_range(row, "AO", "AS")
+                    by_age_female = get_range(row, "AU", "AY")
+                    by_age_male = get_range(row, "BA", "BE")
 
-                by_source = get_range(row, "BF", "BI")
+                    by_source = get_range(row, "BG", "BJ")
 
-                workers = get_range(row, "BK", "BO")
+                    workers = get_range(row, "BL", "BR")
 
-                csvrow = (
-                    [
-                        day_of_outbreak,
-                        date,
-                        pandemic_phase,
-                        tests_performed_cum,
-                        tests_performed_day,
-                        positive_tests_cum,
-                        positive_tests_day,
-                        count_in_care,
-                        count_in_hospital,
-                        count_in_icu,
-                        count_in_icu_bad,
-                        deaths_cum,
-                    ]
-                    + regional_data
-                    + by_age
-                    + by_age_female
-                    + by_age_male
-                    + by_source
-                    + workers
-                )
-                # print(
-                #     len(regional_data),
-                #     len(by_age),
-                #     len(by_age_female),
-                #     len(by_age_male),
-                #     len(by_source),
-                #     len(workers),
-                # )
-                writer.writerow(csvrow)
+                    csvrow = (
+                        [
+                            day_of_outbreak,
+                            date,
+                            pandemic_phase,
+                            tests_performed_cum,
+                            tests_performed_day,
+                            positive_tests_cum,
+                            positive_tests_day,
+                            count_in_care,
+                            count_in_hospital,
+                            count_in_icu,
+                            count_in_icu_bad,
+                            deaths_cum,
+                        ]
+                        + regional_data
+                        + by_age
+                        + by_age_female
+                        + by_age_male
+                        + by_source
+                        + workers
+                    )
+                    writer.writerow(csvrow)
+                except IndexError as e:
+                    print(e)
+                    print(row)
 
 
 if __name__ == "__main__":
