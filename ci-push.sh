afbc73dc873cd7fa030295706aca48385cc1c36a
Travis CI <travis@travis-ci.org>
2020-03-20 15:14:54 +0100

    wip

---
+++ ci-push.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+REPO="github.com/overlordtm/COVID19.si"
+FILES="data/full.csv"
+
+git remote add origin https://${GITHUB_TOKEN}@.git
+
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit() {
+  git checkout master
+  git add ${FILES}
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+push() {
+  git remote add origin https://${GH_TOKEN}@${REPO}.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+setup_git
+commit
+push
